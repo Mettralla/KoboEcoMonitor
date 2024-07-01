a5788a7e63d507e1ef1903bca397fada066fe49f
Mettralla <67493167+Mettralla@users.noreply.github.com>
2024-07-01 02:36:12 -0300

    added create entry

--- app/controllers/green_areas_controller.py
+++ app/controllers/green_areas_controller.py
@@ -1,6 +1,7 @@
 from config import Config
-from flask import jsonify, render_template
+from flask import jsonify, render_template, request, url_for, redirect
 import requests
+from datetime import datetime
 
 class GreenAreasController():
     @classmethod
@@ -17,14 +18,47 @@ class GreenAreasController():
                 "tipo": data["Tipo"],
                 "calle": data["Calle"],
                 "ordenanza": data["Ordenanza"],
-                "tiene_juegos": data["_Tiene_juegos_infantiles"],
-                "tiene_bancos": data["_Tiene_bancos"]
+                "tiene_juegos": data["_Tiene_juegos_infantiles"].lower(),
+                "tiene_bancos": data["_Tiene_bancos"].lower()
             }
             greenZone.append(new_entry)
         
         return render_template("green_areas/index.html", items=greenZone)
         # return jsonify(entry), 200
+        
+    @classmethod
+    def new_green_areas(cls):
+        return render_template("green_areas/new.html")
 
     @classmethod
     def create_green_areas(cls):
-        return render_template("green_areas/index.html")
+        nombre = request.form.get('nombre')
+        tipo = request.form.get('tipo')
+        calle = request.form.get('calle')
+        ordenanza = request.form.get('ordenanza')
+        tiene_juegos = request.form.get('juegos')
+        tiene_bancos = request.form.get('bancos')
+        
+        new_green_area = {
+            "id": Config.PROJECT_UID,
+            "submission": {
+                "formhub/uuid": Config.UUID,
+                "start": datetime.now().isoformat(),
+                "end": datetime.now().isoformat(),
+                "Nombre":nombre,
+                "Tipo": tipo,
+                "Calle": calle,
+                "Ordenanza": ordenanza,
+                "_Tiene_juegos_infantiles": tiene_juegos,
+                "_Tiene_bancos": tiene_bancos,
+            }
+        }
+        
+        try:
+            response = requests.post("https://kc.kobotoolbox.org/api/v1/submissions", json=new_green_area, headers=Config.HEADER)
+            if response.status_code == 201:
+                return redirect(url_for('green_area_bp.get_green_zones'))
+            else:
+                return jsonify({'error': 'Failed to submit data.'}), response.status_code
+        except requests.exceptions.RequestException as e:
+            return jsonify({'error': str(e)}), 500
--- app/routes/green_areas.py
+++ app/routes/green_areas.py
@@ -3,5 +3,6 @@ from ..controllers.green_areas_controller import GreenAreasController
 
 green_areas = Blueprint("green_area_bp", __name__)
 
-green_areas.route("", methods=["GET"])(GreenAreasController.get_green_areas)
-green_areas.route("/create", methods=["GET"])(GreenAreasController.create_green_areas)
+green_areas.route("", methods=["GET"], endpoint="get_green_zones")(GreenAreasController.get_green_areas)
+green_areas.route("/new", methods=["GET"], endpoint="new_green_zones")(GreenAreasController.new_green_areas)
+green_areas.route("/create", methods=["POST"], endpoint="create_green_zones")(GreenAreasController.create_green_areas)
