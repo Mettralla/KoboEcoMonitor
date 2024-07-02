ad868e5a7cb23da61eca09b67f734a934d755fd9
Mettralla <67493167+Mettralla@users.noreply.github.com>
2024-07-01 23:32:36 -0300

    added edit and update endpoints

--- app/controllers/green_areas_controller.py
+++ app/controllers/green_areas_controller.py
@@ -24,8 +24,7 @@ class GreenAreasController():
             greenZone.append(new_entry)
         
         return render_template("green_areas/index.html", items=greenZone)
-        # return jsonify(entry), 200
-        
+
     @classmethod
     def new_green_areas(cls):
         return render_template("green_areas/new.html")
@@ -62,3 +61,51 @@ class GreenAreasController():
                 return jsonify({'error': 'Failed to submit data.'}), response.status_code
         except requests.exceptions.RequestException as e:
             return jsonify({'error': str(e)}), 500
+        
+    @classmethod
+    def edit_green_areas(cls, id):
+        response = requests.get(
+            f"https://kc.kobotoolbox.org/api/v1/data/{Config.FORM_ID}/{id}", headers=Config.HEADER
+        )
+        raw_data = response.json()
+        serialize_data = {
+            "id": raw_data["_id"],
+            "nombre": raw_data["Nombre"],
+            "tipo": raw_data["Tipo"],
+            "calle": raw_data["Calle"],
+            "ordenanza": raw_data["Ordenanza"],
+            "tiene_juegos": raw_data["_Tiene_juegos_infantiles"],
+            "tiene_bancos": raw_data["_Tiene_bancos"]
+        }
+        return render_template("green_areas/edit.html", items=serialize_data)
+
+    @classmethod
+    def update_green_areas(cls, id):
+        nombre = request.form.get('nombre')
+        tipo = request.form.get('tipo')
+        calle = request.form.get('calle')
+        ordenanza = request.form.get('ordenanza')
+        tiene_juegos = request.form.get('juegos')
+        tiene_bancos = request.form.get('bancos')
+
+        payload = {
+            "payload": {
+            "submission_ids": [int(id)],
+            "data": {
+                "Nombre": nombre,
+                "Tipo": tipo,
+                "Calle": calle,
+                "Ordenanza": ordenanza,
+                "_Tiene_juegos_infantiles": tiene_juegos,
+                "_Tiene_bancos": tiene_bancos,
+            }
+                }
+        }
+        
+        response = requests.patch(
+            f"https://kf.kobotoolbox.org/api/v2/assets/{Config.PROJECT_UID}/data/bulk/", json=payload, headers=Config.HEADER)
+        
+        if response.status_code == 200:
+                return redirect(url_for('green_area_bp.get_green_zones'))
+        else:
+            return jsonify({'error': 'Failed to submit data.'}), response.status_code
